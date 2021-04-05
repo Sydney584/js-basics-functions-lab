491d06f844898e969dbe0bef7780d74345b9c3eb
sydney <sydneychase5841@gmail.com>
2021-04-04 22:08:08 -0400

    Updating Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+function distanceFromHqInBlocks(blockNumber) {
+        if (blockNumber > 42) {
+          return blockNumber - 42;
+        } else {
+          return 42 - blockNumber;
+        } 
+}
+     //returns the number of blocks given a value
+     function distanceFromHqInFeet (blockNumber) {
+        return distanceFromHqInBlocks(blockNumber) * 264;
+      }
+      function distanceTravelledInFeet (start, destination) {
+        if (start < destination) {
+          return (destination - start) * 264;
+        } else {
+          return (start - destination) * 264;
+        }
+      }
+   //
+
+
+
+
+function distanceTravelledInFeet(feet) {
+    return distanceTravelledInFeet(43, 48);
+}
+
+function calculatesFarePrice(price) {
+
+}
 // Code your solution in this file!
